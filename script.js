4c7a8c2c3a5bbb4e330d8b2b025d25594e251065
Ibrahim1022 <ibrahimhusseinjama1995@gmail.com>
2024-08-19 19:48:54 +0300

    Done excercise

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+let grade = 85; // Assign a value between 1 and 100
+
+if (grade >= 90 && grade <= 100) {
+    console.log("You got an A grade");
+} else if (grade >= 75 && grade < 90) {
+    console.log("You got a B grade");
+} else if (grade >= 60 && grade < 75) {
+    console.log("You got a C grade");
+} else if (grade < 60) {
+    console.log("You got a FAILED");
+}
